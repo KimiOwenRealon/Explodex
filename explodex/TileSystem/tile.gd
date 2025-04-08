7409130d2575a7eb652d6e03c8985dc31efc1482
KimiRealon <kimi.realonn@gmail.com>
2025-04-09 08:55:41 +1200

    Update tile.gd

--- explodex/TileSystem/tile.gd
+++ explodex/TileSystem/tile.gd
@@ -7,7 +7,7 @@ var sprite_size = 16
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		var local_mouse_pos = get_local_mouse_position()
-		if abs(local_mouse_pos.x) < sprite_size and abs(local_mouse_pos.y) < sprite_size:
+		if abs(local_mouse_pos.x) < sprite_size/2 and abs(local_mouse_pos.y) < sprite_size/2:
 			if event.is_action("left_click"):
 				
 				pass #remove "pass" after adding any code

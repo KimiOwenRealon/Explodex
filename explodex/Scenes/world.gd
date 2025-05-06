1c273732d95b61aae62b957170077ada284449b2
KimiRealon <kimi.realonn@gmail.com>
2025-05-07 08:59:24 +1200

    aw

--- explodex/Scenes/world.gd
+++ explodex/Scenes/world.gd
@@ -1,11 +1,3 @@
 extends Node2D
 #objects
 var t = preload("res://TileSystem/tile.tscn")
-
-#settings
-const tile_size = 16
-var board_size_x = 12
-var board_size_y = 10
-var tiles = {} #tile dictionary
-var mine_array = []
-#sets up initial tile grid

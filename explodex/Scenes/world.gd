dc7786997d29733466c65ef0f223c9d6a5b6c67c
KimiRealon <kimi.realonn@gmail.com>
2025-05-07 08:40:28 +1200

    Update world.gd

--- explodex/Scenes/world.gd
+++ explodex/Scenes/world.gd
@@ -6,15 +6,3 @@ var t = preload("res://TileSystem/tile.tscn")
 const tile_size = 16
 var board_size_x = 12
 var board_size_y = 10
-
-#sets up initial tile grid
-func create_grid():
-	for i in range(0, board_size_x):
-		for j in range(0, board_size_y):
-			var ti = t.instantiate()
-			add_child(ti)
-			var offset = Vector2((board_size_x*tile_size)/2-tile_size/2, (board_size_y*tile_size)/2-tile_size/2)
-			ti.position = Vector2(i*tile_size,j*tile_size) - offset
-
-func _ready():
-	create_grid()

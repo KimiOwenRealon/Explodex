a7d11c74cd2bd977ad7ca16373afb5720cc51dc3
KimiRealon <kimi.realonn@gmail.com>
2025-05-07 08:38:46 +1200

    Update tile.gd

--- explodex/TileSystem/tile.gd
+++ explodex/TileSystem/tile.gd
@@ -2,24 +2,6 @@ extends Node2D
 class_name tile 
 
 var sprite_size = 16
-var revealed = false
-var flagged = false
-
-
-##mouse events for the tile
-func _input(event):
-	if event is InputEventMouseButton and event.pressed:
-		var local_mouse_pos = get_local_mouse_position()
-		if abs(local_mouse_pos.x) < sprite_size and abs(local_mouse_pos.y) < sprite_size:
-			if event.is_action("left_click"):
-				if not revealed and not flagged:
-					reveal()
-				
-			if event.is_action("right_click"):
-				if not revealed:
-					toggle_flag()
-				
-func reveal():
-	revealed = true
-func toggle_flag():
-	flagged = !flagged
+var pos : Vector2
+var bomb : bool = false
+var adjactent_tiles : int
